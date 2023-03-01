26d9863180428bcf85cdd0cd51cbb083ca9d303c
Guilherme Marques Rizzatto Teixeira <guimarquesrz@gmail.com>
2023-02-28 21:36:27 -0300

    Simplified the code and removed the ifs

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("Insert your \nUsername:");
-        String username = keyboard.nextLine().trim();
-        String newUsername;
-        String password;
-        if (username.length() <= 10) {
-            System.out.println("Your username is too short, only: " + username.length() + "\tcaracters");
-            System.out.println("Please do a username with more or equal than 10 caracters");
-            System.out.print("Insert your \nUsername:");
-            newUsername = keyboard.nextLine().trim();
-            if (newUsername.length() >= 10) {
-                System.out.print("Password:");
-                password = keyboard.next().trim();
-                System.out.println("Your username is: " + newUsername + "\tand your password is: " + password);
-            }
-        } else {
-            System.out.print("Password:");
-            password = keyboard.next().trim();
-            System.out.println("Your username is: " + username + "\tand your password is: " + password);
-        }
+        String username = "";
+        String password = "";
 
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Insert your Username: ");
+            username = scanner.nextLine().trim();
+            if (username.length() >= 10)
+                break;
+            System.out.println("Your username is too short, only: " + username.length() + " caracters");
+            System.out.println("Please insert a username with 10 or more caracters");
+        }
+        System.out.print("Insert your password: ");
+        password = scanner.nextLine().trim();
+        System.out.println("Your username is: " + username + "\nand your password is: " + password);
     }
 }
